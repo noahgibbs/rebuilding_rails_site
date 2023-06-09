42a5eac939b8466754a31cd7198b821fb00a631a
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-06-09 23:15:28 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-git push && ssh deployer@madrubyscience.com "cd checkouts/rebuilding_rails_site && git pull"
+echo "Don't need this any more. These days it's all hosted off the repo.
+
+#git push && ssh deployer@madrubyscience.com "cd checkouts/rebuilding_rails_site && git pull"
